49d507aa5e2e2822841fb35e78767e5564051e44
Anamol Sapkota <anamolsapkota@gmail.com>
2019-11-25 20:38:57 +0545

    fifth

--- client.py
+++ client.py
@@ -1,32 +1,40 @@
-import time, socket, sys
-print('Client Server...')
-time.sleep(1)
-#Get the hostname, IP Address from socket and set Port
-soc = socket.socket()
-shost = socket.gethostname()
-ip = socket.gethostbyname(shost)
-#get information to connect with the server
-print(shost, '({})'.format(ip))
-server_host = input('Enter server\'s IP address:')
-name = input('Enter Client\'s name: ')
-port = 1234
-print('Trying to connect to the server: {}, ({})'.format(server_host, port))
-time.sleep(1)
-soc.connect((server_host, port))
-print("Connected...\n")
-soc.send(name.encode())
-server_name = soc.recv(1024)
-server_name = server_name.decode()
-print('{} has joined...'.format(server_name))
-print('Enter [bye] to exit.')
-while True:
-   message = soc.recv(1024)
-   message = message.decode()
-   print(server_name, ">", message)
-   message = input(str("Me > "))
-   if message == "[bye]":
-      message = "Leaving the Chat room"
-      soc.send(message.encode())
-      print("\n")
-      break
-   soc.send(message.encode())
+# Python program to implement client side of chat room. 
+import socket 
+import select 
+import sys 
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+if len(sys.argv) != 3: 
+	print "Correct usage: script, IP address, port number"
+	exit() 
+IP_address = str(sys.argv[1]) 
+Port = int(sys.argv[2]) 
+server.connect((IP_address, Port)) 
+
+while True: 
+
+	# maintains a list of possible input streams 
+	sockets_list = [sys.stdin, server] 
+
+	""" There are two possible input situations. Either the 
+	user wants to give manual input to send to other people, 
+	or the server is sending a message to be printed on the 
+	screen. Select returns from sockets_list, the stream that 
+	is reader for input. So for example, if the server wants 
+	to send a message, then the if condition will hold true 
+	below.If the user wants to send a message, the else 
+	condition will evaluate as true"""
+	read_sockets,write_socket, error_socket = select.select(sockets_list,[],[]) 
+
+	for socks in read_sockets: 
+		if socks == server: 
+			message = socks.recv(2048) 
+			print message 
+		else: 
+			message = sys.stdin.readline() 
+			server.send(message) 
+			sys.stdout.write("<You>") 
+			sys.stdout.write(message) 
+			sys.stdout.flush() 
+server.close() 
+
